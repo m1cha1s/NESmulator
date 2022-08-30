180d2fe8a17167f8b5b67858038cc89cdfaaec0c
m1cha1s <msulej04@gmail.com>
2022-08-30 15:57:42 +0200

    Updated submodules, created a working memory with begginings of the 6502 processor

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,52 @@
 #include <iostream>
-#include <cpu.hpp>
 #include <string>
+#include <memory>
+
+#include <cpu6502.hpp>
+#include <memMap.hpp>
+#include <mem.hpp>
+
+using namespace mulator;
+
+#define MAX_MEM 1024 * 64
+
+class MyMem : public mem::MemMapEntry
+{
+private:
+  uint8_t mem[MAX_MEM];
+
+public:
+  MyMem(size_t start = 0)
+  {
+    this->start = start;
+    end = MAX_MEM;
+  }
+
+  uint8_t get(size_t relativeLoc)
+  {
+    return mem[relativeLoc];
+  }
+
+  void set(size_t relativeLoc, uint8_t val)
+  {
+    mem[relativeLoc] = val;
+  }
+
+  std::string getName()
+  {
+    return "RAM";
+  }
+};
 
 int main()
 {
-  std::cout << greeter("Michal") << std::endl;
+  mem::MemMap memMap;
+  memMap.addEntry<MyMem>();
+
+  mem::Mem mem(memMap);
+
+  std::cout << memMap.dumpEntries();
+
+  cpu6502::CPU6502 cpu(memMap);
+  cpu.reset();
 }
